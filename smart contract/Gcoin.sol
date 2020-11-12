7db1725bcf48c485155c27851b6ea1cfbdfbb9a9
lilyynyang <yyn605@outlook.com>
2020-11-12 19:13:27 +0800

    Gcoin SC update. Add more funtions, use overflow protection library

--- smart contract/Gcoin.sol
+++ smart contract/Gcoin.sol
@@ -1,45 +1,75 @@
-pragma solidity ^0.4.21;
+pragma solidity ^0.4.26;
+
+import "./SafeMath.sol";
 
 contract ERC20Interface {
-  string public name;
-  string public symbol;
-  uint8 public  decimals;
-  uint public totalSupply;
 
-
-  function transfer(address _to, uint256 _value) returns (bool success);
+  function totalSupply() public view returns (uint256);
+  function balanceOf(address _owner) public view returns (uint256 balance);
+  function transfer(address _to, uint256 _value) public returns (bool success);
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
+  function approve(address _spender, uint256 _value) public returns (bool success);
+  function allowance(address _owner, address _spender) public view returns (uint256 remaining);
 
   event Transfer(address indexed _from, address indexed _to, uint256 _value);
-
+  event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 }
 
 contract ERC20 is ERC20Interface {
+    using SafeMath for uint256;
 
-    mapping (address => uint256) public balanceOf;
-    mapping (address => mapping (address => uint256)) internal allowed;
+    // Determined in compile-time, not take up state variable storage space 
+    // -> cheaper & safer
+    string public constant name = "Gcoin";
+    string public constant symbol = "Gcoin";
+    uint8 public constant decimals = 0;
 
-    constructor() public {
-        totalSupply = 100000000000;
-        name = "Gcoin";
-        symbol = "Gcoin";
-        decimals = 0;
-        balanceOf[msg.sender] = totalSupply;
+    uint256 public totalSupply;
+    mapping (address => uint256) private balances;
+    mapping (address => mapping (address => uint256)) private allowed;
+
+    constructor(uint256 _totalSupply) public {
+        totalSupply = _totalSupply;
+        balances[msg.sender] = _totalSupply;
+    }
+    
+    function totalSupply() public view returns (uint256) {
+        return totalSupply;
     }
 
-  function balanceOf(address _owner) view returns (uint256 balance) {
-      return balanceOf[_owner];
-  }
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return balances[_owner];
+    }
 
-    function transfer(address _to, uint _value) public returns (bool success) {
-        require(_to != address(0));
-        require(_value <= balanceOf[msg.sender]);
-        require(balanceOf[_to] + _value >= balanceOf[_to]);
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(_to != address(0), "Cannot transfer to address(0)");
+        require(_value <= balances[msg.sender], "Balance not enough!");
 
-        balanceOf[msg.sender] -= _value;
-        balanceOf[_to] += _value;
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
         emit Transfer(msg.sender, _to, _value);
         return true;
     }
 
-  
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_to != address(0), "Cannot transfer to address(0)");
+        require(_value <= allowed[_from][msg.sender], "Tokens allowed is not enough!");
+        require(_value <= balances[_from], "Balance not enough!");
+        balances[_from] = balances[_from].sub(_value);
+        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
+        return allowed[_owner][_spender];
+    }
 }
