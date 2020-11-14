e20d227e39488cfd9d502416f566fef57001be5a
lilyynyang <yyn605@outlook.com>
2020-11-14 23:25:01 +0800

    restruct project, add js for Gcoin

---
+++ application/app/gcoin.js
@@ -0,0 +1,144 @@
+import Web3 from "web3";
+import GcoinArtifact from "../build/contracts/Gcoin.json";
+
+const Gcoin {
+	web3: null,
+	account: null
+	meta: null,
+
+	start: async function() {
+		const { web3 } = this;
+
+		try {
+			// get contract instance
+			const networkId = await web3.eth.net.getId();
+			const deployedNetwork = GcoinArtifact.networks[networkId];
+			this.meta = new web3.eth.Contract(
+				GcoinArtifact.abi, 
+				deployedNetwork.address,
+			);
+
+			// get account
+			this.setAccount(0);
+			
+		} catch (error) {
+			console.error("Could not connect to contract or chain.");
+		}
+	},
+
+	name: async function() {
+		const { name } = this.meta.methods;
+		contractName = await name().call();
+		
+		// update page
+	}
+
+	symbol: async function() {
+		const { symbol } = this.meta.methods;
+		contractSymbol = await name().call();
+		
+		// update page
+	}
+
+	decimals: async funcntion() {
+		const { decimals } = this.meta.methods;
+		contractDecimals = await decimals().call();
+		
+		// update page
+	}
+
+	totalSupply: async function() {
+		const { totalSupply } = this.meta.methods;
+		supply = await totalSupply().call();
+		
+		// update page
+	}
+
+	balanceOf: async function() {
+		const owner = document.getElementById("balance-owner").value;
+		const { balanceOf } = this.meta.methods;
+		balance = await balanceOf(owner).call();
+		
+		// update page
+	}
+
+	transfer: async function() {
+		const to = document.getElementById("tranfer-to").value;
+		const value =  parseInt(document.getElementById("tranfer-value").value);
+
+		this.setStatus("Initiating transaction... (please wait)");
+
+		const { transfer } = this.meta.methods;
+		await transfer(to, value).call();
+
+		this.setStatus("Transaction complete!");
+		this.refreshBalance();
+	}
+
+	transferFrom: async function() {
+		const from = document.getElementById("delegate-from").value;
+		const to = document.getElementById("delegate-to").value;
+		const value = parseInt(document.getElementById("delegate-value").value);
+
+		this.setStatus("Initiating transaction... (please wait)");
+
+		const { transferFrom } = this.meta.methods;
+		await transferFrom(from, to, value).call();
+
+		this.setStatus("Transaction complete!");
+		this.refreshBalance();
+	}
+
+	approve: async function() {
+		const spender = document.getElementById("approve-spender").value;
+		const value = parseInt(document.getElementById("approve-alue").value);
+
+		this.setStatus("Processing... (please wait)");
+
+		const { approve } = this.meta.methods;
+		await approve(spender, value).call();
+
+		this.setStatus("Complete!");
+	}
+
+	allowance: async funcntion() {
+		const spender = document.getElementById("allowance-spender").value;
+		const owner = document.getElementById("allowance-owner").value;
+
+		const { allowance } = this.meta.methods;
+		await allowance(owner, spender).call();
+	}
+
+	refreshBalance: async function() {
+		const { balanceOf } = this.meta.methods;
+		balance = await balanceOf(this.account).call();
+
+		// update page
+		const balanceElements = document.getElementsById("balance");
+    	balanceElements.innerHTML = balance;
+	}
+
+	setAccount: async function(idx) {
+		const accounts = await web3.eth.getAccounts();
+		if (idx <= accounts.length && idx >= 0) {
+			this.account = accounts[idx];
+		} else {
+			this.account = accounts[0];
+		}
+	}
+
+	setStatus: function(message) {
+		// update page
+		const status = document.getElementById("status");
+		status.innerHTML = message;
+	}
+
+}
+
+window.addEventListner("load", function() {
+	Gcoin.web3 = new Web3(
+		new Web3.providers.HttpProvider("http://127.0.0.1:7545"),
+	);
+
+	Gcoin.start();
+})
