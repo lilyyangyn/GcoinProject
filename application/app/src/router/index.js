a63f2b8578a32a3e86eaaf6bd13ec68cef970283
lilyynyang <yyn605@outlook.com>
2021-01-08 16:19:37 +0800

    solve warning in router rendering

--- application/app/src/router/index.js
+++ application/app/src/router/index.js
@@ -46,7 +46,7 @@ const routes = [
 
       {
         path: '/explorer',
-        name: 'Explorer',
+        //name: 'Explorer',
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
