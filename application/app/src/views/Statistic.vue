7937a77a8f7d9f79d69918809cc41d9c76f33ea8
NicholsonTsang <u3547738@connect.hku.hk>
2021-02-18 15:25:03 +0800

    adjust statistic page's UI

--- application/app/src/views/Statistic.vue
+++ application/app/src/views/Statistic.vue
@@ -34,16 +34,37 @@
 <!--</style>-->
 
 <template>
-  <iframe  height ="100%" width="100%"
-           style="border:3px solid black;"
-           src="http://localhost:3000"  id="frame" ></iframe>
+<!--  <iframe  height ="100%" width="100%"-->
+<!--           style="border:3px solid black;"-->
+<!--           src="http://localhost:3000"  id="frame" ></iframe>-->
+  <div>
+<!--    <div id="frame-wrapper" hidden="" style="position: absolute; height: 1000px; width: 886px; transform: scale(0.5); transform-origin: 0px 0px; display: block;">-->
+<!--      <iframe id="frame" style="position: absolute; width: 3000px; height: 100%; border: none;" src="http://localhost:3000"></iframe>-->
+<!--    </div>-->
+    <div id="frame-wrapper" hidden="" style="position: absolute; height: 100%; width: 100%; transform: scale(0.65); transform-origin: 0px 0px; display: block;">
+      <iframe id="frame" style="position: absolute; width: 2066px; height: 1077px; border: none; margin-left: 5px" src="http://localhost:3000"></iframe>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  mounted() {
+    // if ($(this).attr('src') != '') { resize(); $('#frame-wrapper').fadeIn(300); }
+    if(document.getElementById("frame")!=null){
+      resize();
+      // this.getElementById("frame-wrapper").fadeIn(300);
+
+    }
+  }
+}
+</script>
 
 <style>
 
-#frame {
-  width: 97%;
-  height: 100%;
-  resize: both;
-}
+/*#frame {*/
+/*  width: 97%;*/
+/*  height: 100%;*/
+/*  resize: both;*/
+/*}*/
 </style>
