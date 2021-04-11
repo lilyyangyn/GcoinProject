ca5ee05ead62d0123e3f3484eb062be5d9de128d
NicholsonTsang <u3547738@connect.hku.hk>
2021-04-11 21:32:14 +0800

    change UI

--- application/app/src/views/CompanyRegister.vue
+++ application/app/src/views/CompanyRegister.vue
@@ -2,7 +2,7 @@
   <div >
     <h1 style="text-align: center">Company Register</h1>
     <Tabs type="card" class="card-container">
-      <TabPane label="withdraw">
+      <TabPane label="Company Account Registration">
         <Card style="width:100%">
           <Form  label-position="left" :label-width="70" class="form-container">
             <FormItem label="Address">
